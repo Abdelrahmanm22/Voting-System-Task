16c92f799bc737cb6ee0d6771b5ad004e5ec6c3a
Abdelrahmanm22 <abdra1396@gmail.com>
2025-03-20 00:55:50 +0200

    setup admin panel using adminlte dashboard and aplly laravel blade template inheritance

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix'=>'dashboard'],function(){
+    Route::get('/home',[\App\Http\Controllers\Web\AdminController::class,'index'])->name('home');
+});
